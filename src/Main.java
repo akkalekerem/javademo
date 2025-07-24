d2ae158e59b5c966c096f1de3f34816260a0461c
Kerem Akkale <keremakkale@gmail.com>
2025-07-24 14:42:29 +0300

    To do list çalışmasında CRUD'un neredeyse tüm aşamalırının temelini Java üzerinden öğrenmeye çalıştım.

--- src/Main.java
+++ src/Main.java
@@ -32,6 +32,41 @@ public class Main {
         for (int i = 0; i< todoList.size(); i++){
             System.out.println((i+1)+ ". " +todoList.get(i));
         }
+
+        //Görev Silme
+        System.out.print("Silmek istediğiniz görevin numarasını giriniz (yoksa 0): ");
+        int deleteIndex = scanner.nextInt();
+        scanner.nextLine();//Dummy read
+
+        if (deleteIndex > 0 && deleteIndex <= todoList.size()) {
+            String removedTask = todoList.remove(deleteIndex - 1);
+            System.out.println("Silinen görev: " + removedTask);
+        } else if (deleteIndex == 0) {
+            System.out.println("Silme işlemi yapılmadı.");
+        } else {
+            System.out.println("Geçersiz numara tuşladınız.");
+        }
+
+        // Görev güncelleme
+        System.out.print("Güncellemek istediğiniz görevin numarasını yazın: ");
+        int  updateIndex = scanner.nextInt();
+        scanner.nextLine();//dummy read
+         if (updateIndex > 0 && updateIndex <= todoList.size()) {
+             System.out.print("Yeni görev metnini girin: ");
+             String updatedTask = scanner.nextLine();
+             todoList.set(updateIndex - 1, updatedTask);
+             System.out.println("Görev güncellendi.");
+         } else if (updateIndex == 0) {
+             System.out.println("Güncelleme yapılmadı.");
+         } else {
+             System.out.println("Geçersiz numara!");
+         }
+        //Güncel görev listesi
+        System.out.println("Güncel görevler: ");
+        for (int i = 0; i< todoList.size(); i++){
+            System.out.println((i+1)+ ". " +todoList.get(i));
+        }
+
         scanner.close();
     }
 }

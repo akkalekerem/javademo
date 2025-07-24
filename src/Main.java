843c24a0fbbf4e439e863d406b91447d4b195fa0
Kerem Akkale <keremakkale@gmail.com>
2025-07-24 12:30:15 +0300

    To do list çalışmasında CRUD'un C Create (Yeni Görev Ekleme) işlemini yapıp commit ettim.

--- src/Main.java
+++ src/Main.java
@@ -9,24 +9,23 @@ public class Main {
         todoList.add("Java öğren");
         todoList.add("Spring Boot öğren");
 
-        System.out.println("Güncel görevler:");
+        //Görevleri göster
+        System.out.println("Görevler:");
         for (int i = 0; i < todoList.size(); i++){
             System.out.println((i + 1) + ". " + todoList.get(i));
         }
 
-        //Görev güncelleme
-        System.out.print("Düzenlemek istediğiniz görevin numarasını giriniz: ");
-        int indexToUpdate = scanner.nextInt();
-        scanner.nextLine(); //BU KISIM ÇOK ÖNEMLİ YAPMAYI UNUTMA
+        //Yeni görev ekleme
+        System.out.print("Yeni görev giriniz(Boş bırakılırsa eklenmez): ");
+        String newTask = scanner.nextLine();
 
-        if (indexToUpdate > 0 && indexToUpdate <= todoList.size()){
-            System.out.print("Yeni görev Başlığını giriniz: ");
-            String newTask = scanner.nextLine();
-            todoList.set(indexToUpdate - 1, newTask);
-            System.out.println("Görev güncellendi.");
+        if (!newTask.trim().isEmpty()){
+            todoList.add(newTask);//Görevi listeye ekle
+            System.out.println("Yeni görev eklendi.");
         } else {
-            System.out.println("Geçersiz bir görev numarası tuşladınız. ");
+            System.out.println("Boş görev eklenmedi.");
         }
+
 
         //Güncel görev listesi
         System.out.println("Güncel görevler: ");
